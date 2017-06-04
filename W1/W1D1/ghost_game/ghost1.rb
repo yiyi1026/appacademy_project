7f9ebd982b61647db3f36e9d8c685771e2d4d768
yiyi1026 <vivian.li.uibe@gmail.com>
2017-06-04 12:01:20 -0700

    refining ghost

--- W1/W1D1/ghost_game/ghost1.rb
+++ W1/W1D1/ghost_game/ghost1.rb
@@ -2,26 +2,31 @@
 class Game
   @@record = Hash.new{0}
   attr_reader :dictionary
-  attr_accessor :players, :fragment, :loser_hash
+  attr_accessor :players, :fragment, :loser_hash, :loser_hash
 
   def initialize(players, fragment, dictionary)
     @players = players
     @fragment = fragment
     @dictionary = dictionary
+    @loser_hash = Hash.new(0)
   end
 
   def run
-
+    until loser_hash.any?{|k,v| v == 5}
+      play_round
+      losses(previous_player)
+      @fragment = ''
+    end
   end
+
   def play_round
+    puts "\nA new turn!"
     until loss?
       puts "It is your turn, #{current_player.name}!"
       take_turn(current_player)
       next_player!
       puts "Let's switch"
     end
-
-    losses(previous_player)
   end
 
   def current_player
@@ -33,7 +38,7 @@ class Game
   end
 
   def next_player!
-    @players = players[1..-1] + players[0]
+    @players = [players[1] , players[0]]
   end
 
   def take_turn(player)
@@ -56,11 +61,14 @@ class Game
     dictionary.any? {|word,_| word.start_with?(string)}
   end
 
-  def self.losses(player)
-    loser_hash ||= Hash.new(0)
+  def losses(player)
+
     loser_hash[player] += 1
     if loser_hash[player] == 5
-      puts "Game over! #{player} is the GHOST"
+      puts "Game over! #{player.name} is the GHOST"
+    else
+      puts "This round is over and #{player.name} loses this round."
+      puts "current status: " + current_player.name + ":" + loser_hash[current_player].to_s + " " + previous_player.name + ":" + loser_hash[previous_player].to_s
     end
   end
 
@@ -91,6 +99,7 @@ class Player
   def guess
     puts "Give me a letter!"
     gets.chomp
+    # 'd'
   end
 
   def alert_invalid_guess
@@ -107,8 +116,9 @@ if __FILE__ == $PROGRAM_NAME
     a = line.chomp
     dictionary[a] = true
   end
-  game = Game.new(players,'a', dictionary)
-  game.take_turn(mary)
+  game = Game.new(players,'', dictionary)
+
+  game.run
 end
 
 
