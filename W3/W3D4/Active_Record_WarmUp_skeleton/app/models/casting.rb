1b6cbd685b3860f862bea1623cd3addb56abaab0
yiyi1026 <vivian.li.uibe@gmail.com>
2017-06-14 20:55:17 -0700

    initial upload W3D4 homework

---
+++ W3/W3D4/Active_Record_WarmUp_skeleton/app/models/casting.rb
@@ -0,0 +1,17 @@
+class Casting < ActiveRecord::Base
+
+	belongs_to :actor
+	# shorthand for:
+	# belongs_to :actor,
+	# class_name: 'Actor',
+	# foreign_key: :actor_id,
+	# primary_key: :id
+
+	belongs_to :movie
+	# shorthand for:
+	# belongs_to :movie,
+	# class_name: 'Movie',
+	# foreign_key: :movie_id,
+	# primary_key: :id
+
+end
