ff10b4e6dd09485962f2d7bca40cae8c55ba5f52
yiyi1026 <vivian.li.uibe@gmail.com>
2017-06-30 00:28:34 -0700

    initial commit W5D5 homework

--- W5/W5D4/callback_and_function_call_practice.js
+++ W5/W5D4/callback_and_function_call_practice.js
@@ -1,11 +1,11 @@
-window.setTimeout(function() {
-  alert('HAMMERTIME');
-}, 5000);
+// window.setTimeout(function() {
+//   alert('HAMMERTIME');
+// }, 5000);
 
 function hammerTime(time) {
-  window.setTimeout((time) => {
+  window.setTimeout(function() {
     console.log(`${time} is hammertime!`);
-  });
+  }, time)
 }
 
 const readline = require('readline');
@@ -16,21 +16,18 @@ const reader = readline.createInterface({
 });
 
 function teaAndBiscuits() {
-  let first, second;
 
-  reader.question('Would you like some tea?', (res) => {
-    first = res;
+  reader.question('Would you like some biscuits?', function(res) {
     console.log(`You replied ${res}.`);
-    reader.question('Would you like some biscuits?', (res) => {
-      second = res;
-      console.log(`You replied ${res}.`);
+    reader.question('Would you like some tea?', (res_two) => {
+      console.log(`You replied ${res_two}.`);
+
+      const firstRes = (res === 'yes') ? 'do' : 'don\'t';
+      const secondRes = (res_two === 'yes') ? 'do' : 'don\'t';
+
+      console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+      reader.close();
     });
-    const firstRes = (first === 'yes') ? 'do' : 'don\'t';
-    const secondRes = (second === 'yes') ? 'do' : 'don\'t';
-    console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
-    reader.close();
   });
-  // window.setTimeout(question1, 1000);
-
-
 }
+teaAndBiscuits()
---
+++ W5/W5D5/argExercises/practise.js
@@ -0,0 +1,15 @@
+function sum(...args) {
+  let sum = 0;
+  args.forEach((el) => {
+    sum += el
+  })
+  return sum;
+}
+
+console.log(sum(1, 3, 5, 3, 7));
+
+Function.prototype.myBind = function(obj, ) {
+
+};
+
+}
