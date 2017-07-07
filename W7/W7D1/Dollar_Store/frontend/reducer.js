8ca95dd13990d46be317fcd043544d48969b5678
yiyi1026 <vivian.li.uibe@gmail.com>
2017-07-07 13:11:11 -0700

    initial commit W7D1 homework

---
+++ W7/W7D1/Dollar_Store/frontend/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  baseCurrency: "Please select",
+  rates: {}
+};
+
+const reducer = (state = initialState, action) => {
+  return state; // remove this and fill out the body of the reducer function
+};
+
+export default reducer;
