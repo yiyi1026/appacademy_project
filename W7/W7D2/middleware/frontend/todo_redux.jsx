60dedb9294a5bd50f96e959210d16d7f7afb1ede
yiyi1026 <vivian.li.uibe@gmail.com>
2017-07-10 22:06:41 -0700

    complete W7D2 homework Middleware phase 2

--- W7/W7D2/middleware/frontend/todo_redux.jsx
+++ W7/W7D2/middleware/frontend/todo_redux.jsx
@@ -4,10 +4,38 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+//phase 1
+const addLoggingToDispatch = (store)=>{
+  const next = store.dispatch;
+
+  return (action)=>{
+    console.log(store.getState());
+    console.log(action);
+    next(action);
+    console.log(store.getState());
+  }
+}
+
+//phase 2
+// const addLoggingToDispatch = store => next => action => {
+//   const dispatch = store.dispatch;
+//
+//
+//
+//   if (typeof action === 'function'){
+//
+//     return next(action);
+//   }
+//   return
+// };
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
+  let store = configureStore(preloadedState);
+
+  //phase 1
+  store.dispatch = addLoggingToDispatch(store);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
