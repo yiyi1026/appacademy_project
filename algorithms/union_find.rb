c174d269fca9c40c913daab3a6d8fd667cf9c68c
yiyi1026 <vivian.li.uibe@gmail.com>
2017-09-06 14:40:43 -0700

    add union find

---
+++ algorithms/union_find.rb
@@ -0,0 +1,45 @@
+class UnionFind
+
+  attr_reader :store
+  def initialize(n)
+    @store = (0..n).map{|val|Node.new(val)}
+  end
+
+  def union(p, q)
+    # add connection between p and q
+    # merge components containing p and g, change all entries
+    # whose id equals id[p] to id[q]
+    # change first one to match the second one
+
+    old_id = p.id
+    new_id = q.id
+    @store.each do |node|
+      node.id = new_id if node.val == old_id
+    end
+
+  end
+
+  def connected?(p, q)
+    # if p and q are in the same component
+    p.id == q.id
+  end
+
+  def find(p)
+    # time complexity O(1)    
+    p.id
+  end
+
+  def count
+
+  end
+
+end
+
+class Node
+  attr_accessor :id
+  def initialize(val)
+    @val = val
+    @id = val
+  end
+
+end
